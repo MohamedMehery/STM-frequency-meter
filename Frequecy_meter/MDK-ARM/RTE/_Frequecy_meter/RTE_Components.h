47d3220429a55940377a2d7ebe405e8a13bc71b4
MohamedA.Mehery <49691904+MohamedMehery@users.noreply.github.com>
2021-12-14 16:28:33 +0200

    Init

---
+++ Frequecy_meter/MDK-ARM/RTE/_Frequecy_meter/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Frequecy_meter' 
+ * Target:  'Frequecy_meter' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
